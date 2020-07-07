7dfe2707d4f80f0634a5708a438ac0906c5934ab
nkans <34555932+nkans@users.noreply.github.com>
2020-07-07 12:26:11 -0700

    Update constants.py

--- constants.py
+++ constants.py
@@ -3,3 +3,9 @@ password = "SuperSecret1@3!"
 azure_client_id="70761111-81ef-46ea-ae22-d768babe1111" # Azure Active Directory client id
 
 azure_access_token = "1111jtnufwl5d3ne4hh772uqvbjuyldaue74s5zhlak56gaj1111"
+
+pat = "d152cd5081869778e245807795401f69426cbc79"
+
+gh_pat = "d152cd5081869778e245807795401f69426cbc11"
+
+github_token = "1111cd5081869778e245807795401f69426c1111"
